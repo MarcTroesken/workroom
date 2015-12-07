132a27c104ad6b7210f26dcd7790e84c63fb846c
Marc Troesken <m.troesken@mail.de>
2015-12-07 14:44:22 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo apt-add-repository ppa:nginx/development
 
 sudo apt-get update
 
-sudo apt-get install -y nginx php7.0-fpm php7.0-cli php7.0-common mysql-server php7.0-mysql php7.0-json php7.0-opcache php7.0-phpdbg php7.0-dbg php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-dev php7.0-intl git-core sqlite3 redis-server
+sudo apt-get install -y nginx php7.0-fpm php7.0-cli php7.0-common php7.0-curl mysql-server php7.0-mysql php7.0-json php7.0-opcache php7.0-phpdbg php7.0-dbg php7.0-gd php7.0-imap php7.0-tidy php7.0-dev php7.0-intl git-core sqlite3 redis-server
 
 sudo service php7.0-fpm restart
 
